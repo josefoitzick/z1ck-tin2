61c25d8d58dd68de5737ca2d98abd6a7598d6b22
z1ck <jose.foitzick@usach.cl>
2024-11-28 15:06:45 -0300

    Se inicia paso a microservicios

---
+++ ms-creditsimulation/src/main/java/com/z1ck/ms_creditsimulation/MsCreditsimulationApplication.java
@@ -0,0 +1,13 @@
+package com.z1ck.ms_creditsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreditsimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCreditsimulationApplication.class, args);
+	}
+
+}
---
+++ ms-creditsimulation/src/test/java/com/z1ck/ms_creditsimulation/MsCreditsimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z1ck.ms_creditsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditsimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
