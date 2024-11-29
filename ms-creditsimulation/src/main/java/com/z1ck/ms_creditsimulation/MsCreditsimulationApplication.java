0d92a8881428e65f896d6c14baa8e56c57b9ddcc
z1ck <jose.foitzick@usach.cl>
2024-11-29 00:08:21 -0300

    ms-simulation configuracion correcta

--- ms-creditsimulation/src/main/java/com/z1ck/ms_creditsimulation/MsCreditsimulationApplication.java
+++ ms-creditsimulation/src/main/java/com/z1ck/ms_creditsimulation/MsCreditsimulationApplication.java
@@ -2,12 +2,10 @@ package com.z1ck.ms_creditsimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class MsCreditsimulationApplication {
 
 	public static void main(String[] args) {
