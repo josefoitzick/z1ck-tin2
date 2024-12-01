60be93141f56bc759fb1ab8795cdf42fc498d0b4
z1ck <jose.foitzick@usach.cl>
2024-11-30 21:42:44 -0300

    folder ms-credit

---
+++ ms-credit/src/test/java/com/z1ck/ms_credit/MsCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z1ck.ms_credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
