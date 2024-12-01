12570be41b0cc0522e0bca8cfe8c22bca82a105d
z1ck <jose.foitzick@usach.cl>
2024-11-30 21:07:59 -0300

    ms-client

---
+++ ms-client/src/test/java/com/z1ck/ms_client/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z1ck.ms_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
